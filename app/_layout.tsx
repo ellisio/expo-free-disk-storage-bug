6206b2c0c5e0c7cc6ba657cd65d3de2f422da276
Andrew Ellis <127468+ellisio@users.noreply.github.com>
2024-06-11 09:29:13 -0600

    chore: add disk check
    Signed-off-by: Andrew Ellis <127468+ellisio@users.noreply.github.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,8 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import * as FileSystem from 'expo-file-system';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -12,15 +13,41 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
+  const [loaded, setLoaded] = useState(false);
+  const [diskChecked, setDiskChecked] = useState(false);
+  const [fontLoaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+
+  useEffect(() => {
+    const diskCheck = async () => {
+      const free = await FileSystem.getFreeDiskStorageAsync();
+      const total = await FileSystem.getTotalDiskCapacityAsync();
+
+      console.log('Disk space:', {
+        free: free / 1000000000,
+        total: total / 1000000000,
+      });
+
+      setDiskChecked(true);
+    };
+
+    diskCheck();
+  }, []);
+
+  useEffect(() => {
+    if (fontLoaded && diskChecked) {
+      setLoaded(true);
+    }
+  }, [fontLoaded, diskChecked]);
 
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
     }
   }, [loaded]);
+
+
 
   if (!loaded) {
     return null;
